e6cc6974f5602fe3a6b471d8f8cb8274791b4611
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-04-27 17:50:37 +0530

    fix: connect to production database with SSL

--- db/pool.js
+++ db/pool.js
@@ -1,4 +1,10 @@
 const { Pool } = require("pg");
 const DATABASE_URL = process.env.DATABASE_URL;
 
-module.exports = new Pool({ connectionString: DATABASE_URL });
+module.exports = new Pool({
+    connectionString: DATABASE_URL,
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+});
