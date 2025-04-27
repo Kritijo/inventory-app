d541581353df1cc951f9a9c292568ef28842aa5b
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-04-27 17:54:35 +0530

    fix: change db url

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.EXTERNAL_URL;
 
 module.exports = new Pool({
     connectionString: DATABASE_URL,
